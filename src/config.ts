0ef06b81b984210a41e211b0e804845cb7e5fd1e
Mxcoc <37839658+Mxcoc@users.noreply.github.com>
2025-12-07 12:57:51 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ import type {
 
 //--- Readme Page Config ---
 export const SITE: Site = {
-  title: 'Cattk's Blog',
+  title: 'Cattk Blog',
   description:
     'Litos is a modern blogging theme built on Astro.js, designed for developers. It supports multiple post layouts, photo displays, project displays, and more, providing an elegant user experience and powerful customization capabilities.',
   website: 'https://litos.vercel.app/',
